0e991cda345af95a33363afdee3847d84ec949e9
JVHP <jvhp162001@gmail.com>
2021-07-10 14:10:02 -0300

    Titulo dos aplicativos, alterados

--- App5_Cadastro_OLD/app/src/main/java/com/jvhp/app5_cadastro/DadosUsuario.kt
+++
@@ -1,7 +0,0 @@
-package com.jvhp.app5_cadastro
-
-import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
-
-@Parcelize
-data class DadosUsuario(val nome: String, val idade: Int) : Parcelable
